3692ec5eacd99142ffd2237f839ff301a1e44c79
Suuringo <72997519+Suuringo@users.noreply.github.com>
2022-05-16 23:44:49 +0200

    Always use format 140 (aac) when getting audio stream URL from youtube-dl
    Clipper took the last audio-only format when running youtube-dl -g,
    which doesn't always guarentee that the format is .aac and made ffmpeg
    crash.

--- clipper/clipper.go
+++ clipper/clipper.go
@@ -1,7 +1,6 @@
 package clipper
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"os/exec"
@@ -16,23 +15,16 @@ type Clipper struct {
 }
 
 func (c *Clipper) Run() error {
-	ytCmd := exec.Command("youtube-dl", "-g", "https://www.youtube.com/watch?v="+c.YoutubeId)
-	ytLinks, err := ytCmd.Output()
+	ytCmd := exec.Command("youtube-dl", "-g", "https://www.youtube.com/watch?v="+c.YoutubeId, "-f", "140")
+	audioStreamURL, err := ytCmd.Output()
 	if err != nil {
 		return fmt.Errorf("error running yt-dl to get video stream & audio stream links: %w", err)
-	}
-
-	// [0] is video stream, [1] is audio stream
-	streams := strings.Split(string(ytLinks), "\n")
-
-	if len(streams) < 2 {
-		return errors.New("expected two streams from youtube-dl -g command")
 	}
 
 	ffmpegArgs := []string{
 		"-y",
 		"-ss", fmt.Sprintf("%.2f", c.StartSecond),
-		"-i", streams[1],
+		"-i", strings.TrimSpace(string(audioStreamURL)),
 		"-t", fmt.Sprintf("%.2f", c.EndSecond-c.StartSecond),
 		"-c", "copy",
 		fmt.Sprintf("%s.aac", c.OutputName),
